7b8644f7a54e5608919735dbae9fd313ab17eb80
Alek-Grujic <alek.grujic001@outlook.com>
2025-08-09 10:31:59 +0200

    Refactor and rewrite Pig Game codebase for improved functionality and structure

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
+// selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
 const diceEl = document.querySelector('.dice');
-const currentNum1 = document.querySelector('#current--0');
-let current = 0;
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
-// buttons variables
-const rollDiceBtn = document.querySelector('.btn--roll');
-const holdBtn = document.querySelector('.btn--hold');
-const newGameBtn = document.querySelector('.btn--new');
-
+// starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-rollDiceBtn.addEventListener('click', function () {
-  let rollTheDice = Math.trunc(Math.random() * 6) + 1;
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// rilling dice functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
   diceEl.classList.remove('hidden');
-  diceEl.src = `images/dice-${rollTheDice}.png`;
-  if (rollTheDice === 1) {
-    current = 0;
-    currentNum1.textContent = current;
+  diceEl.src = `images/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    current += rollTheDice;
-    currentNum1.textContent = current;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
-  console.log(current);
 });
