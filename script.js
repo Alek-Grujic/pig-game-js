083dd3e4a830ace9fc64f11d7476663a196f964d
Alek-Grujic <alek.grujic001@outlook.com>
2025-08-09 09:13:09 +0200

    Switch active player and reset current score when dice roll is 1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const currentNum1 = document.querySelector('#current--0');
+let current = 0;
 
 // buttons variables
 const rollDiceBtn = document.querySelector('.btn--roll');
@@ -14,7 +15,14 @@ diceEl.classList.add('hidden');
 
 rollDiceBtn.addEventListener('click', function () {
   let rollTheDice = Math.trunc(Math.random() * 6) + 1;
-  currentNum1.textContent = rollTheDice;
   diceEl.classList.remove('hidden');
   diceEl.src = `images/dice-${rollTheDice}.png`;
+  if (rollTheDice === 1) {
+    current = 0;
+    currentNum1.textContent = current;
+  } else {
+    current += rollTheDice;
+    currentNum1.textContent = current;
+  }
+  console.log(current);
 });
