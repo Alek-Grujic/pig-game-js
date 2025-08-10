ab201c16ca5c6dc56024151ae6c9ef860906fd20
Alek-Grujic <alek.grujic001@outlook.com>
2025-08-10 15:15:55 +0200

    Add hold score functionality and switchPlayer function for style toggle

--- script.js
+++ script.js
@@ -20,7 +20,15 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
-// rilling dice functionality
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+// rolling dice functionality
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -32,10 +40,16 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+// hold the current score functionality
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  switchPlayer();
 });
